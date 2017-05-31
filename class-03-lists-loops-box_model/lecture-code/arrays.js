a5558c540c64fec622b9bd9ac6e5491303879bca
awallraff <adam.wallraff@gmail.com>
2017-05-30 20:21:47 -0700

    added class 3 lecture code

--- class-03-lists-loops-box_model/lecture-code/arrays.js
+++ class-03-lists-loops-box_model/lecture-code/arrays.js
@@ -1,5 +1,52 @@
 'use strict';
 
 //ARRAYS DEMONSTRATION
-console.log('test');
+
+var name = 'Adam';
+
+var names = ['Adam', 'Rachel', 'Tyler', 'Jeremiah', 'Kyle'];
+
+var instructorName = names[3];
+
+console.log(instructorName);
+
+var mixedTypes = ['Adam', 31, names];
+
+//before
+console.log(mixedTypes);
+
+
+console.log(mixedTypes);
+
+//remove names array
+var namesIndex = mixedTypes.indexOf(names);
+console.log(namesIndex);
+
+//using splice to insert
+mixedTypes.splice(1, 0, 'Wallraff');
+
+//using splice to remove
+mixedTypes.splice(namesIndex, 1);
+
+//after change
+console.log(mixedTypes);
+console.log(mixedTypes.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
