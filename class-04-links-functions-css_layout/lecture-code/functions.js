acdcec3ec3ddbbf3986404e97cb67ece1a084639
awallraff <adam.wallraff@gmail.com>
2017-06-05 18:50:53 -0700

    added lecture code and lab-5

---
+++ class-04-links-functions-css_layout/lecture-code/functions.js
@@ -0,0 +1,50 @@
+'use strict';
+
+//Function Declaration
+function addTwoThings(thing1, thing2) {
+  var total = thing1 + thing2;
+  var results = [thing1, thing2, total];
+  return results;
+}
+
+var total1 = addTwoThings(1,5);
+var total2 = addTwoThings(11,157);
+var total1 = addTwoThings(1,5);
+
+var results = addTwoThings(5, 10);
+
+var newTotal = results;
+
+var string1 = results[0];
+var string2 = results[1];
+
+addTwoThings('hello, ', 'my name is Adam.');
+
+something();
+var total = anotherWayToAdd2Things(20, 200);
+
+
+//Function Expression
+var anotherWayToAdd2Things = function(thing1, thing2){
+  return thing1 + thing2;
+};
+
+function somthing(){
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
