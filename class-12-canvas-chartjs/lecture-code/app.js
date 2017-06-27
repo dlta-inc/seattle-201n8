7a8afabb3b996e6cf76d267dba886ea6fcca185d
awallraff <adam.wallraff@gmail.com>
2017-06-26 21:25:51 -0700

    add lecture demo code

---
+++ class-12-canvas-chartjs/lecture-code/app.js
@@ -0,0 +1,100 @@
+'use strict';
+
+var context = document.getElementById('my-chart').getContext('2d');
+
+var dataSet = [12, 6, 24, 5, 3, 9];
+var itemNames = ['clock', 'watch', 'bigfoot', 'apple', 'TV', 'Blingy Red Shoes'];
+var chartColors = ['black', 'silver', 'brown', 'green', 'blue', 'red'];
+
+var myChart = new Chart(context, {
+  type: 'bar',
+  data: {
+    labels: itemNames,
+    dataSets: [{
+      label: '# of Votes',
+      data: dataSet,
+      backgroundColor: chartColors
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
+
+var dataObject =  {
+  labels: itemNames,
+  dataSets: [{
+    label: '# of Votes',
+    data: dataSet,
+    backgroundColor: chartColors
+  }]
+};
+
+var chartOptions = {
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero: true
+      }
+    }]
+  }
+};
+
+
+var myChart = new Chart(context, {
+  type: 'bar',
+  data: dataObject,
+  options: chartOptions
+});
+
+
+function Dog(name, breed, color){
+  this.name = name;
+  this.breed = breed;
+  this.color = color;
+}
+
+Dog.prototype.bark = function(){
+  console.log('Hello! ' + this.name + ' woof!');
+};
+
+var myDog = new Dog('Mika', 'mix', 'black/brown');
+var yourDog = new Dog('Cid', 'pitmix', 'tan');
+
+
+myDog.bark();
+yourDog.bark();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
